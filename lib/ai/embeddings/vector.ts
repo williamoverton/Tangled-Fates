d66bf252127d4a3a40e76d5ce9b40d2818c86367
Will Overton <will@execify.ai>
2025-10-20 19:42:34 +0100

    make embeddings type safe

--- lib/ai/embeddings/vector.ts
+++ lib/ai/embeddings/vector.ts
@@ -1,15 +1,75 @@
-import { WorldEvent } from "@/lib/game/types";
 import { Index } from "@upstash/vector";
+import dedent from "dedent";
+import { z } from "zod";
 
-type WorldEventMetadata = {
-  when: string;
-  where: string;
-  what: string;
-};
+// Things that can happen in the world
+const WorldEventMetadata = z.object({
+  type: z.literal("world_event"),
+  when: z.string(),
+  where: z.string(),
+  what: z.string(),
+});
 
-const index = new Index<WorldEventMetadata>();
+// Places in the world
+const WorldLocationMetadata = z.object({
+  type: z.literal("world_location"),
+  placeType: z
+    .string()
+    .describe(
+      "The type of place, such as a village, city, forest, mansion, tavern, mineral cave, etc."
+    ),
+  name: z
+    .string()
+    .describe(
+      "The name of the place. Such as 'Mistral Village', 'The Forgotten Caves', 'The Royal Palace', etc."
+    ),
+  description: z
+    .string()
+    .describe(
+      "A detailed description of the place. Include everything needed to describe the place."
+    ),
+  location: z
+    .string()
+    .describe(
+      "The location of the place in the world. Explain where it is in the world and what it is near."
+    ),
+});
 
-async function upsert(data: string, metadata: WorldEventMetadata) {
+const WorldPersonalityMetadata = z.object({
+  type: z.literal("world_personality"),
+  personalityType: z
+    .string()
+    .describe(
+      "The type of personality, such as a merchant, blacksmith, wizard, villian etc."
+    ),
+  name: z
+    .string()
+    .describe(
+      "The name of the personality. Such as 'John Doe', 'Jane Smith', 'The Wizard', etc."
+    ),
+  description: z
+    .string()
+    .describe(
+      "A detailed description of the personality. Include everything needed to describe the personality."
+    ),
+});
+
+export const Metadata = z.discriminatedUnion("type", [
+  WorldEventMetadata,
+  WorldLocationMetadata,
+  WorldPersonalityMetadata,
+]);
+
+// Inferred TypeScript types
+export type WorldEventMetadata = z.infer<typeof WorldEventMetadata>;
+export type WorldLocationMetadata = z.infer<typeof WorldLocationMetadata>;
+export type WorldPersonalityMetadata = z.infer<typeof WorldPersonalityMetadata>;
+export type Metadata = z.infer<typeof Metadata>;
+export type QueryType = Metadata["type"];
+
+const index = new Index<Metadata>();
+
+async function upsert(data: string, metadata: Metadata) {
   await index.upsert({
     id: crypto.randomUUID(),
     data: data,
@@ -17,31 +77,62 @@ async function upsert(data: string, metadata: WorldEventMetadata) {
   });
 }
 
-async function queryIndex(data: string) {
+async function queryIndex(data: string, type: QueryType) {
   return await index.query({
     data: data,
     topK: 10,
     includeVectors: true,
     includeMetadata: true,
+    filter: `type = "${type}"`,
   });
 }
 
-export const addWorldEvent = async (event: WorldEvent) => {
-  const content = `
+export const addWorldEvent = async (event: WorldEventMetadata) => {
+  const content = dedent`
     In ${event.where} at ${event.when}, ${event.what} happened.
   `;
 
-  await upsert(content, {
-    what: event.what,
-    when: event.when,
-    where: event.where,
-  });
+  await upsert(content, event);
 };
 
 export const getWorldEvents = async (query: string) => {
-  const res = await queryIndex(query);
+  const res = await queryIndex(query, "world_event");
   return res.map((item) => ({
-    metadata: item.metadata,
+    worldEvent: WorldEventMetadata.parse(item.metadata),
+    score: item.score,
+  }));
+};
+
+export const addWorldLocation = async (location: WorldLocationMetadata) => {
+  const content = dedent`
+    In ${location.location} there is a ${location.placeType} called ${location.name}. It can be described as: ${location.description}
+  `;
+
+  await upsert(content, location);
+};
+
+export const getWorldLocations = async (query: string) => {
+  const res = await queryIndex(query, "world_location");
+  return res.map((item) => ({
+    worldLocation: WorldLocationMetadata.parse(item.metadata),
+    score: item.score,
+  }));
+};
+
+export const addWorldPersonality = async (
+  personality: WorldPersonalityMetadata
+) => {
+  const content = dedent`
+    There is a ${personality.personalityType} called ${personality.name}. They can be described as: ${personality.description}
+  `;
+
+  await upsert(content, personality);
+};
+
+export const getWorldPersonalities = async (query: string) => {
+  const res = await queryIndex(query, "world_personality");
+  return res.map((item) => ({
+    worldPersonality: WorldPersonalityMetadata.parse(item.metadata),
     score: item.score,
   }));
 };
--- lib/game/types.ts
+++
@@ -1,9 +0,0 @@
-import { z } from "zod/v4";
-
-export const WorldEventSchema = z.object({
-  when: z.iso.datetime(),
-  where: z.string(),
-  what: z.string(),
-});
-
-export type WorldEvent = z.infer<typeof WorldEventSchema>;
